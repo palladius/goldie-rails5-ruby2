f82a86f7d44d62a16504a9e61efe91fdf59901f6
Riccardo Carlesso <palladiusbonton+derek.zrh.corp.google.com@gmail.com>
2018-10-28 14:38:44 +0100

    added license and route for it. v0.9.1

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,6 @@ class WelcomeController < ApplicationController
 
   def about
   end
+  def license
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/index'
+  get 'welcome/license'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
